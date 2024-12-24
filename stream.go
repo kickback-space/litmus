c256eb161481cb62cea9b66fd9d4773019209b25
rocco <rocco@kickback.space>
2024-12-23 17:21:18 -0800

    space to tabs

--- stream.go
+++ stream.go
@@ -2,112 +2,112 @@
 package litmus
 
 import (
-    "context"
-    "crypto/rand"
-    "encoding/binary"
-    "time"
+	"context"
+	"crypto/rand"
+	"encoding/binary"
+	"time"
 
-    "github.com/pion/webrtc/v3"
-    . "github.com/blitz-frost/log"
+	"github.com/pion/webrtc/v3"
+	. "github.com/blitz-frost/log"
 )
 
 const (
-    headerSize      = 12
-    maxTestDuration = 200 * time.Second
+	headerSize      = 12
+	maxTestDuration = 200 * time.Second
 )
 
 func stream(ctx context.Context, dc *webrtc.DataChannel, connID string, testDone chan struct{}, testError chan error, networkTuner *NetworkTuner, peerConnection *webrtc.PeerConnection) {
-    startTime := time.Now()
-    sequence := uint32(0)
+	startTime := time.Now()
+	sequence := uint32(0)
 
-    ticker := time.NewTicker(time.Millisecond * 100)
-    defer ticker.Stop()
+	ticker := time.NewTicker(time.Millisecond * 100)
+	defer ticker.Stop()
 
-    defer func() {
-        dc.Close()
-        close(testDone)
-        peerConnection.Close()
-    }()
+	defer func() {
+		dc.Close()
+		close(testDone)
+		peerConnection.Close()
+	}()
 
-    calculatePacketRate := func(bitrate int) (packetSize int, packetsPerSecond int) {
-        packetSize = defaultPacketSize
-        bitsPerPacket := packetSize * 8
-        packetsPerSecond = (bitrate * 1000) / bitsPerPacket
-        return
-    }
+	calculatePacketRate := func(bitrate int) (packetSize int, packetsPerSecond int) {
+		packetSize = defaultPacketSize
+		bitsPerPacket := packetSize * 8
+		packetsPerSecond = (bitrate * 1000) / bitsPerPacket
+		return
+	}
 
    var lastBufferedAmount uint64
    var totalBytesSent uint64
    lastCheckTime := time.Now()
 
-    for {
-        select {
-        case <-ctx.Done():
-            return
-        case <-ticker.C:
-            if networkTuner.IsTestComplete() {
-                Log(Info, "Network testing complete", Entry{"connID", connID})
-                return
-            }
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			if networkTuner.IsTestComplete() {
+				Log(Info, "Network testing complete", Entry{"connID", connID})
+				return
+			}
 
-            currentBitrate := networkTuner.getCurrentBitrate()
-            packetSize, packetsPerSecond := calculatePacketRate(currentBitrate)
+			currentBitrate := networkTuner.getCurrentBitrate()
+			packetSize, packetsPerSecond := calculatePacketRate(currentBitrate)
 
-            if packetsPerSecond > 0 {
-                newInterval := time.Second / time.Duration(packetsPerSecond)
-                ticker.Reset(newInterval)
-            }
+			if packetsPerSecond > 0 {
+				newInterval := time.Second / time.Duration(packetsPerSecond)
+				ticker.Reset(newInterval)
+			}
 
-            packet := make([]byte, packetSize)
-            binary.BigEndian.PutUint32(packet[0:headerSize-8], sequence)
-            binary.BigEndian.PutUint64(packet[headerSize-8:headerSize], uint64(time.Now().UnixNano()))
+			packet := make([]byte, packetSize)
+			binary.BigEndian.PutUint32(packet[0:headerSize-8], sequence)
+			binary.BigEndian.PutUint64(packet[headerSize-8:headerSize], uint64(time.Now().UnixNano()))
 
-            if _, err := rand.Read(packet[headerSize:]); err != nil {
-                Log(Error, "Failed to generate random data",
-                    Entry{"error", err},
-                    Entry{"connID", connID})
-                testError <- err
-                return
-            }
+			if _, err := rand.Read(packet[headerSize:]); err != nil {
+				Log(Error, "Failed to generate random data",
+					Entry{"error", err},
+					Entry{"connID", connID})
+				testError <- err
+				return
+			}
 
-            if err := dc.Send(packet); err != nil {
-                Log(Error, "Failed to send test packet",
-                    Entry{"error", err},
-                    Entry{"connID", connID})
-                testError <- err
-                return
-            }
-            totalBytesSent += uint64(packetSize)
+			if err := dc.Send(packet); err != nil {
+				Log(Error, "Failed to send test packet",
+					Entry{"error", err},
+					Entry{"connID", connID})
+				testError <- err
+				return
+			}
+			totalBytesSent += uint64(packetSize)
 
-            sequence++
+			sequence++
 
-            currentBuffered := dc.BufferedAmount()
+			currentBuffered := dc.BufferedAmount()
 
-            elapsed := time.Since(lastCheckTime).Milliseconds()
-            // 200 matches the adaptInterval in network.go and metrics manager
-            if elapsed >= 200 {
-                // Calculate actual bytes transmitted (accounting for buffer changes)
-                bufferChange := int64(currentBuffered) - int64(lastBufferedAmount)
-                actualBytesSent := int64(totalBytesSent)
-                
-                // If buffer decreased, those bytes were sent too
-                if bufferChange < 0 {
-                    actualBytesSent += -bufferChange
-                }
-                
-                effectiveSentBitsPerSec := float64(actualBytesSent) * 8.0 / (float64(elapsed)/1000.0)
-                networkTuner.SetServerEffectiveRate(effectiveSentBitsPerSec)
+			elapsed := time.Since(lastCheckTime).Milliseconds()
+			// 200 matches the adaptInterval in network.go and metrics manager
+			if elapsed >= 200 {
+				// Calculate actual bytes transmitted (accounting for buffer changes)
+				bufferChange := int64(currentBuffered) - int64(lastBufferedAmount)
+				actualBytesSent := int64(totalBytesSent)
+				
+				// If buffer decreased, those bytes were sent too
+				if bufferChange < 0 {
+					actualBytesSent += -bufferChange
+				}
+				
+				effectiveSentBitsPerSec := float64(actualBytesSent) * 8.0 / (float64(elapsed)/1000.0)
+				networkTuner.SetServerEffectiveRate(effectiveSentBitsPerSec)
 
-                lastBufferedAmount = currentBuffered
-                totalBytesSent = 0
-                lastCheckTime = time.Now()
-            }
+				lastBufferedAmount = currentBuffered
+				totalBytesSent = 0
+				lastCheckTime = time.Now()
+			}
 
 
-            if time.Since(startTime) >= maxTestDuration {
-                Log(Info, "Max test duration reached", Entry{"connID", connID})
-                return
-            }
-        }
-    }
+			if time.Since(startTime) >= maxTestDuration {
+				Log(Info, "Max test duration reached", Entry{"connID", connID})
+				return
+			}
+		}
+	}
 }
